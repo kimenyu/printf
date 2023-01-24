d92a35ef649348428285ff51c419378285126c25
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-24 08:58:29 +0300

    chore: test main.c

--- src/main.c
+++ src/main.c
@@ -7,7 +7,12 @@
  */
 int main(void)
 {
-	_printf("Printing a simple sentence\n");
+	int len1, len2;
+	
+	len1 = _printf("Printing a simple sentence\n");
+	len2 = printf("Printing a simple sentence\n");
+
+	printf("[%d, %d]", len1, len2);
 
 	return (0);
 }
