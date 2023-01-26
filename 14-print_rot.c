e96d01fe10fdf8c483630277208006bfcd9eed8a
Akuya Ekorot <akuyaekorot@gmail.com>
2023-01-26 12:44:35 +0300

    fix betty

--- 14-print_rot.c
+++ 14-print_rot.c
@@ -8,27 +8,27 @@
  */
 int print_rot(va_list ap)
 {
-  char *s = va_arg(ap, char *);
-  int count, i, j;
-  char *letter = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char *encode = " nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+char *s = va_arg(ap, char *);
+int count, i, j;
+char *letter = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char *encode = " nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-  for (i = 0, count = 0; *(s + i) != '\0'; i++)
-  {
-    for (j = 0; *(letter + j) != '\0'; j++)
-    {
-      if (!(s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-      {
-        _putchar(s[i]);
-        break;
-      }
-      if (*(s + i) == *(letter + j))
-      {
-        count += _putchar(*(encode + j));
-        break;
-      }
-    }
-  }
+for (i = 0, count = 0; *(s + i) != '\0'; i++)
+{
+for (j = 0; *(letter + j) != '\0'; j++)
+{
+if (!(s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+{
+_putchar(s[i]);
+break;
+}
+if (*(s + i) == *(letter + j))
+{
+count += _putchar(*(encode + j));
+break;
+}
+}
+}
 
-  return (count);
+return (count);
 }
